62bb1b81832d17d9b0a43b993c3339e86eee8451
vero <vero.boulnois@gmail.com>
2017-03-14 15:44:44 +0100

    ajout commentaires

--- js/login.js
+++ js/login.js
@@ -1,17 +1,22 @@
 $(function(){
-  /*je déclare variable regex pour le nom afin de verifier si des tirets ou des lettres*/
+  //je déclare variable regex pour le nom afin de verifier si des tirets ou des lettres
   var regexName = /^\D+[-]?\D+/i;
-  /*je declare variable regex mail(qui recherche la synthaxe d'une adresse mail) */
+  //je declare variable regex mail(qui recherche la synthaxe d'une adresse mail)
   var regexMail = /^[\w\-\.]+[a-z0-9]@[\w\-\.]+[a-z0-9]\.[a-z]+/i;
+  //je declare variable regex adresse qui recherche synthaxe adresse mail)
   var regexAddress = /^\d{1,5}([ ]?bis|[ ]?ter)?[,]?[ ]\w{2,9}[a-z ]+\d{5}\D+/i;
+  //je démarre ma fonction au click du bouton Ok
   $("#validForm").click(function(){
+    /*je teste la valeur de mon champs pseudo avec une regex qui prend en compte les caractères alphanumériques,
+     si fausse cela affiche croix rouge et cela cache le tick vert */
     if (!regexName.test($('#pseudo').val())){
       $(".pseudo .glyphicon-remove").show();
       $(".pseudo .glyphicon-ok").hide();
+      //alors si vraie cela affiche le tick vert sinon si fausse affiche la croix rouge
     }else{
       $(".pseudo .glyphicon-ok").show();
       $(".pseudo .glyphicon-remove").hide();
-    }
+    }//je teste la valeur dans le champs age si faux(!) qu'il soit numérique ou inférieur à 18 ans
     if (!$.isNumeric($("#age").val()) || $("#age").val() < 18){
       $(".age .glyphicon-remove").show();
       $(".age .glyphicon-ok").hide();
@@ -26,20 +31,20 @@ $(function(){
       $(".address .glyphicon-ok").show();
       $(".address .glyphicon-remove").hide();
     }
-    /*si le test de la regex confirme synthaxe mail fausse*/
     if (!regexMail.test($('#email').val())){
       $(".email .glyphicon-remove").show();
       $(".email .glyphicon-ok").hide();
     }else{
       $(".email .glyphicon-ok").show();
       $(".email .glyphicon-remove").hide();
-    }
+    }//je teste la valeur dans le champs password qu'il ait une longueur de 4 caractères
     if($("#password").val().length < 4){
       $(".password .glyphicon-remove").show();
       $(".password .glyphicon-ok").hide();
     }else{
       $(".password .glyphicon-ok").show();
       $(".password .glyphicon-remove").hide();
+      //je teste que la valeur dans le champs confirm est égale à la valeur du champs password
       if($("#password").val() != $("#confirm").val()){
         $(".confirm .glyphicon-remove").show();
         $(".confirm .glyphicon-ok").hide();
