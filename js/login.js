abff1c36be2244033c9364a0cfc1f8248a3e2978
vero <vero.boulnois@gmail.com>
2017-03-15 11:49:15 +0100

    ajout formulaire connexion

--- js/login.js
+++ js/login.js
@@ -5,8 +5,9 @@ $(function(){
   var regexMail = /^[\w\-\.]+[a-z0-9]@[\w\-\.]+[a-z0-9]\.[a-z]+/i;
   //je declare variable regex adresse qui recherche synthaxe adresse mail)
   var regexAddress = /^\d{1,5}([ ]?bis|[ ]?ter)?[,]?[ ]\w{2,9}[a-z ]+\d{5}\D+/i;
-  //je sauve ma structure json dans une variable
+  //je déclare ma variable users qui va servir à stocker la structure json
   var users = 0;
+  //je déclare ma fonction permettant de lire un fichier
   function readTextFile(file)
   {
     var rawFile = new XMLHttpRequest();
@@ -23,13 +24,18 @@ $(function(){
     }
     rawFile.send(null);
   }
+  //j'appelle la fonction qui va lire mon fichier json
   readTextFile( 'js/users.json' );
+  //je déclare ma fonction qui permet de vérifier si un pseudo ou email existe déjà
   function hasMatchInput(type, inputVal) {
+    //je déclare un booléen pour connaître
     var hasMatch =false;
+    //je crée une boucle de la taille de ma structure json
     for (var index = 0; index < users.user.length; index++) {
       var user = users.user[index];
+      //si le json dont la clé est email ou pseudo correscpond à mon input alors existe déjà il stoppe la fonction
       if(user[type] == inputVal){
-        hasMatch = true;
+        hasMatch = index;
         break;
       }
     }
@@ -39,10 +45,9 @@ $(function(){
   $("#validForm").click(function(){
     //variable qui vérifie que tous mes champs sont remplis correctement
     var isCorrect = true;
-    console.log(users);
     /*je teste la valeur de mon champs pseudo avec une regex qui prend en compte les caractères alphanumériques,
     si fausse cela affiche croix rouge et cela cache le tick vert */
-    if (!regexName.test($('#pseudo').val()) || hasMatchInput("pseudo", $('#pseudo').val())){
+    if (!regexName.test($('#pseudo').val()) || hasMatchInput("pseudo", $('#pseudo').val()) !== false){
       $(".pseudo .glyphicon-remove").show();
       $(".pseudo .glyphicon-ok").hide();
       isCorrect = false;
@@ -67,7 +72,7 @@ $(function(){
       $(".address .glyphicon-ok").show();
       $(".address .glyphicon-remove").hide();
     }
-    if (!regexMail.test($('#email').val()) || hasMatchInput("email", $('#email').val())){
+    if (!regexMail.test($('#email').val()) || hasMatchInput("email", $('#email').val())!== false){
       $(".email .glyphicon-remove").show();
       $(".email .glyphicon-ok").hide();
       isCorrect = false;
@@ -94,13 +99,39 @@ $(function(){
     }
     if(isCorrect){
       //je crée un nouvel utilisateur (objet pour le Json)
-      var newUser = {
+      users.user[users.user.length] = {
         "pseudo": $("#pseudo").val(),
         "age": $("#age").val(),
         "address": $("#address").val(),
         "email": $("#email").val(),
         "password": $("#password").val()
       };
+      //tout est correct donc la fenêtre se ferme
+      $("#signIt").modal("toggle")
+    }
+  });
+  //je démarre ma fonction au click du bouton ok (formulaire connexion) idem formulaire inscription
+  $("#validFormUp").click(function(){
+    var hasMatchPseudo = hasMatchInput("pseudo", $("#pseudoUp").val());
+    var isCorrect = true;
+    if(hasMatchPseudo === false)
+    {
+      $(".pseudoUp .glyphicon-remove").show();
+      $(".pseudoUp .glyphicon-ok").hide();
+    }
+    else {
+      $(".pseudoUp .glyphicon-ok").show();
+      $(".pseudoUp .glyphicon-remove").hide();
+      var hasMatchPassword = users.user[hasMatchPseudo].password;
+      if($("#passwordUp").val() == hasMatchPassword)
+      {
+        $("#signUp").modal("toggle");
+      }
+      else
+      {
+        $(".passwordUp .glyphicon-remove").show();
+        $(".passwordUp .glyphicon-ok").hide();
+      }
     }
   });
 });
